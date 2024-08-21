35c4eb39e73443be4f1eb9bba822b6e70327646f
MMomcilovic <miladin.momcilovic@gmail.com>
2024-08-21 22:38:43 +0200

    fix: test build

--- src/test/java/com/devops/accomodation_service/AccomodationServiceApplicationTests.java
+++ src/test/java/com/devops/accomodation_service/AccomodationServiceApplicationTests.java
@@ -1,15 +1,36 @@
 package com.devops.accomodation_service;
 
+import com.devops.accomodation_service.repository.AccomodationRepository;
+import com.devops.accomodation_service.service.AccomodationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 class AccomodationServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@InjectMocks
+	private AccomodationService accomodationService;
+
+	@Mock
+	private AccomodationRepository accomodationRepository;
+
+	@BeforeEach
+	void setUp() {
+
+	}
+
+	@Test
+	void testGetAccomodation() {
+
+	}
 }
